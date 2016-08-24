8e9a980f353f5200b88f5e7c8020b939ced18cb9
Kartikeya Sharma <09kartikeya@gmail.com>
2016-08-24 19:29:29 +0530

    Added comments

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ for i in x:
     print i,
 print
 
-print "changing x[1] to 3"
+print "changing x[1] to 4"
 if(x[1]==2):
-    x[1]=3
+    x[1]=4
 
 print x[1]
+
+print "Another list y:"
+#created a list of strings y
+y=['a','b']
+
+print y
+#used * operator on y to see its effect
+y=y*2
+
+print y
