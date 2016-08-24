90c8f99e77c02f01034aeb248ad25f60a51ee37a
Kartikeya Sharma <09kartikeya@gmail.com>
2016-08-24 19:12:19 +0530

    Initial commit after initing the git repositary

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+
+print "This is the start message"
+
+x=[1,2,3,4,5]
+print 'x has:'
+for i in x:
+    print i,
+print
+
+print "changing x[1] to 3"
+if(x[1]==2):
+    x[1]=3
+
+print x[1]
